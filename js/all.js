0037cb6caa7e5e2485faf9df8604cf42776ad6ca
Chek <79972532+zz22558822@users.noreply.github.com>
2023-06-21 15:29:21 +0800

    上傳

---
+++ js/all.js
@@ -0,0 +1,31 @@
+// 檢查瀏覽器是否支援 Web NFC API
+if ('NDEFReader' in window) {
+	const statusElement = document.getElementById('status');
+  
+	// 啟用 NFC 功能
+	const reader = new NDEFReader();
+  
+	// 設定事件監聽器
+	reader.addEventListener('reading', event => {
+	  // 讀取標籤資料
+	  const records = event.message.records;
+	  
+	  // 處理標籤資料
+	  for (const record of records) {
+		console.log(record.data);
+	  }
+  
+	  // 更新狀態顯示
+	  statusElement.innerHTML = '<i class="fas fa-tag"></i> NFC 狀態：已讀取';
+	});
+  
+	// 啟動讀取
+	reader.scan().catch(error => {
+	  console.error('Error scanning NFC: ', error);
+	});
+  } else {
+	console.error('Web NFC API is not supported in this browser.');
+	// 更新狀態顯示
+	document.getElementById('status').innerHTML = '<i class="fas fa-exclamation-circle"></i> NFC 功能不支援';
+  }
+  
